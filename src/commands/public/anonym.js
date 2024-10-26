d55f1c3df2d42159b9c54ef97be12abe2f32a3e6
Plhume <72684309+Plhume@users.noreply.github.com>
2024-10-26 10:18:39 +0200

    Add anonym logs

--- src/commands/public/anonym.js
+++ src/commands/public/anonym.js
@@ -2,6 +2,7 @@ const { isMessageInstance } = require('@sapphire/discord.js-utilities');
 const { Command } = require('@sapphire/framework');
 const { CommandInteraction, EmbedBuilder, Colors } = require('discord.js');
 const config = require('../../configs/config');
+const logger = require('../../helpers/logger');
 
 class PublicCommand extends Command {
     constructor(context, options) {
@@ -40,6 +41,33 @@ class PublicCommand extends Command {
         })
 
         const mentions = interaction.options.getString('message').match(/<@!?\d+>/g);
+
+        const channel = await this.container.client.guilds.fetch(config.defaultGuildId[0]).then(guild => guild.channels.fetch(config.channels.anonymLogs))
+        const messageLink = `[Voir le message](https://discord.com/channels/${interaction.guild.id}/${interaction.channel.id}/${interaction.id})`
+        const logEmbed = new EmbedBuilder({
+            title: '🔒 HeavenLogs > Anonymes',
+            description: `Un message anonyme a été envoyé par <@${interaction.user.id}>`,
+            fields: [
+                {
+                    name: 'Message',
+                    value: `\`\`${interaction.options.getString('message')}\`\``,
+                    inline: false
+                },
+                {
+                    name: 'Lien du message',
+                    value: messageLink,
+                    inline: false
+                }
+            ],
+            footer: {
+                text: 'HeavenBot | All rights reserved',
+                iconURL: this.container.client.user.displayAvatarURL()
+            },
+            timestamp: new Date(),
+            color: Colors.DarkButNotBlack
+        })
+
+        await channel.send({ embeds: [logEmbed] })
 
         await interaction.channel.send({
             content: mentions ? mentions.join(' ') : '',
